e24de9bccb069b7cdbc7b41546cd1324ed6a6f46
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2017-04-17 02:58:07 +0300

    Necessary dependencies were added. Eureka and Zuul configurations ware added.

--- src/main/java/com/epam/petclinic/zuul/PetClinicZuulApplication.java
+++ src/main/java/com/epam/petclinic/zuul/PetClinicZuulApplication.java
@@ -2,8 +2,16 @@ package com.epam.petclinic.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableWebMvc
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class PetClinicZuulApplication {
 
     public static void main(String[] args) {
