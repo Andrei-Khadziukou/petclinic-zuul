316011a28f974ef6c045c17683ba10730959ab1e
Andrei-Khadziukou <Andrei_Khadziukou@epam.com>
2017-04-17 01:47:53 +0300

    Gradle support was added

---
+++ src/main/java/com/epam/petclinic/zuul/PetClinicZuulApplication.java
@@ -0,0 +1,12 @@
+package com.epam.petclinic.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicZuulApplication.class, args);
+    }
+}
